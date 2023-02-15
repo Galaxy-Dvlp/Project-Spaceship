ecdcfe458e4875cd0f21babae917495e12f1d690
Gabriel Neves Oliveira Quintão <gabrieloliveiraquintao@gmail.com>
2023-02-15 13:59:15 -0300

    Adição tela do Chamado TIN01

---
+++ login.js
@@ -0,0 +1,4 @@
+function login(){
+    var nome = document.getElementById("usuario").value;/*PEGA O ELEMENTO PELO ID, COPIA O VALOR E ATRIBUI NA VARIAVEL*/
+    var senha = document.getElementById("senha").value;/*PEGA O ELEMENTO PELO ID, COPIA O VALOR E ATRIBUI NA VARIAVEL*/
+}
